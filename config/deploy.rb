acef5e66c431ba41be3407c261f82eef2ec9420f
Jason Garber <jason@sixtwothree.org>
2018-01-07 16:31:03 -0500

    Update gems

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock '3.10.0'
+lock '3.10.1'
 
 set :application, 'whoistheorchid.com'
 set :repo_url, 'git@github.com:jgarber623/whoistheorchid.com.git'
