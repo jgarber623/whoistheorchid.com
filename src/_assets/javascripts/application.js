44474188ab54d6e4776b498c262bc9fc8048e791
Jason Garber <jason@sixtwothree.org>
2018-03-25 10:46:05 -0400

    Revert "Remove UTF-8 characters"
    This reverts commit ae211f77b74b3da1016e9443b6db409cc952a7c5.

--- src/_assets/javascripts/application.js
+++ src/_assets/javascripts/application.js
@@ -1,10 +1,10 @@
 (function() {
   'use strict';
 
-  // Modifying prototypes ¯\_(Y O L O)_/¯
+  // Modifying prototypes… ¯\_(Y O L O)_/¯
   NodeList.prototype.forEach = Array.prototype.forEach;
 
-  // Set <iframe> `src`
+  // Set <iframe> `src`…
   document.querySelectorAll('iframe[data-src]').forEach(function(iframe) {
     iframe.src = iframe.getAttribute('data-src') + location.href;
   });
