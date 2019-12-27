ec7c9bd83a573a5f59d92b0b1f2bd397971fdfeb
Jason Garber <jason@sixtwothree.org>
2019-12-27 15:23:30 -0500

    Add new/updated configuration files

---
+++ .eleventy.js
@@ -0,0 +1,25 @@
+const yaml = require('js-yaml');
+
+module.exports = eleventy => {
+  eleventy.setBrowserSyncConfig(require('./bs-config.js'));
+
+  eleventy.setLibrary('md', require('./lib/libraries/markdown.js'));
+
+  eleventy.addDataExtension('yml', contents => yaml.safeLoad(contents));
+
+  eleventy.addFilter('display_date', require('./lib/filters/display_date.js'));
+  eleventy.addFilter('prettify', require('./lib/filters/prettify.js'));
+
+  eleventy.addPassthroughCopy('./src/assets/images');
+  eleventy.addPassthroughCopy('./src/assets/javascripts');
+  eleventy.addPassthroughCopy('./src/favicon.ico');
+  eleventy.addPassthroughCopy('./src/robots.txt');
+
+  return {
+    dir: {
+      input: './src',
+      layouts: '_layouts',
+      output: './public'
+    }
+  };
+};
---
+++ bs-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  serveStatic: ['public'],
+  serveStaticOptions: {
+    extensions: ['html']
+  }
+};
