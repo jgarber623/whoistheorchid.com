f0b4dfd2b8b85f434538055a56d6d87051885756
Jason Garber <jason@sixtwothree.org>
2023-02-22 23:04:45 -0500

    Refactor and simplify Netlify Build plugin

--- netlify/plugins/github-repository-dispatch/index.js
+++ netlify/plugins/github-repository-dispatch/index.js
@@ -1,14 +1,16 @@
-import * as process from 'node:process';
-import { Octokit } from 'octokit';
+const process = require('node:process');
+const { Octokit } = require('octokit');
 
 const { GITHUB_ACCESS_TOKEN } = process.env;
 
-export const onSuccess = async () => {
-  const octokit = new Octokit({ auth: GITHUB_ACCESS_TOKEN });
+module.exports = {
+  onSuccess: async () => {
+    const octokit = new Octokit({ auth: GITHUB_ACCESS_TOKEN });
 
-  await octokit.request('POST /repos/{owner}/{repo}/dispatches', {
-    owner: 'jgarber623',
-    repo: 'whoistheorchid.com',
-    event_type: 'process-activitypub-outbox'
-  });
+    await octokit.request('POST /repos/{owner}/{repo}/dispatches', {
+      owner: 'jgarber623',
+      repo: 'whoistheorchid.com',
+      event_type: 'process-activitypub-outbox'
+    });
+  }
 };
