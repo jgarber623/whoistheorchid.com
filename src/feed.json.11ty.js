a737d494f6ed31769123d5df33ef10385b14e347
Jason Garber <jason@sixtwothree.org>
2024-02-03 21:52:15 -0500

    Minor tweaks to JSON feed template

--- src/feed.json.11ty.js
+++ src/feed.json.11ty.js
@@ -1,7 +1,7 @@
 export default class {
   data() {
     return {
-      permalink: '/feed.json'
+      permalink: 'feed.json'
     };
   }
 
@@ -10,12 +10,12 @@ export default class {
       collections
         .post
         .map(post => {
-          const url = `${app.start_url}${post.url}`;
+          const id = new URL(post.url, app.start_url);
 
           /* eslint-disable sort-keys */
           return {
-            id: url,
-            url,
+            id,
+            url: id,
             content_html: post.content.trim(),
             content_text: post.rawInput.trim(),
             date_published: post.date
@@ -24,14 +24,14 @@ export default class {
         })
         .reverse();
 
-    const icon = `${app.start_url}${app.icons[2].src}`;
+    const icon = new URL(app.icons[2].src, app.start_url);
 
     /* eslint-disable sort-keys */
     return JSON.stringify({
       version: 'https://jsonfeed.org/version/1.1',
       title: `Updates from ${app.name}`,
       home_page_url: app.start_url,
-      feed_url: `${app.start_url}${permalink}`,
+      feed_url: new URL(permalink, app.start_url),
       description: app.description,
       icon,
       authors: [
