fee9a07212fe3a95f0b4be6d8e90a34aaa895834
Jason Garber <jason@sixtwothree.org>
2025-01-02 19:55:54 -0500

    Use import assertions and config export

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,14 +1,14 @@
-import fs from "node:fs/promises";
-
 import markdownItHandle from "markdown-it-handle";
 
 import liquidPlugin from "@jgarber/eleventy-plugin-liquid";
 import markdownPlugin from "@jgarber/eleventy-plugin-markdown";
 import postcssPlugin from "@jgarber/eleventy-plugin-postcss";
 
+import manifest from "./src/manifest.webmanifest.json" with { type: "json" };
+
 export default async function(eleventyConfig) {
   // Global Data
-  eleventyConfig.addGlobalData("app", JSON.parse(await fs.readFile("./src/manifest.webmanifest.json")));
+  eleventyConfig.addGlobalData("app", manifest);
 
   // Passthrough File Copy
   eleventyConfig
@@ -37,10 +37,10 @@ export default async function(eleventyConfig) {
   });
 
   eleventyConfig.addPlugin(postcssPlugin);
-
-  return {
-    dir: {
-      input: "./src",
-    },
-  };
 }
+
+export const config = {
+  dir: {
+    input: "./src",
+  },
+};
